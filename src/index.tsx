1f1b802328a12f38947a43a11f2a20dde68b8e67
Hardik Jain <jainhardik120@gmail.com>
2024-07-24 19:10:56 +0530

    imported necessary css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import "react-grid-layout/css/styles.css"
+import "react-resizable/css/styles.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
