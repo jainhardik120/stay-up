053236ed0147430f78e0624f3718824001f2473d
Hardik Jain <jainhardik120@gmail.com>
2024-07-24 19:10:14 +0530

    Added react grid layouts basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import React from "react";
+import GridLayout from "react-grid-layout";
+
+class MyFirstGrid extends React.Component {
+  render() {
+    // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 }
+    ];
+    return (
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div key="a" className="bg-green-500">a</div>
+        <div key="b" className="bg-blue-500">b</div>
+        <div key="c" className="bg-red-500">c</div>
+      </GridLayout>
+    );
+  }
+}
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <MyFirstGrid />
+    </div>
   )
 }
